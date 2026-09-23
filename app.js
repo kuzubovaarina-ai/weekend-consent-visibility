bdb87a8e2876b020464dec087fa31c74251f031e
kuzubovaarina-ai <kuzubovaarina@gmail.com>
2026-09-23 14:44:06 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,78 @@
 (function () {
 
+    "use strict";
+
+
+    function show(data) {
+
+        document.body.innerHTML = `
+        <div style="
+            padding:20px;
+            font-family:Arial;
+            color:#333;
+        ">
+            <h2>Bitrix24 Debug</h2>
+
+            <pre>
+${JSON.stringify(data, null, 2)}
+            </pre>
+        </div>
+        `;
+
+    }
+
+
+    show({
+        step: "start",
+        bx24: typeof BX24
+    });
+
+
+
+    if (typeof BX24 === "undefined") {
+
+        show({
+            error: "BX24 object not found"
+        });
+
+        return;
+
+    }
+
+
+
+    BX24.init(function () {
+
+
+        show({
+            step: "BX24 init success"
+        });
+
+
+
+        BX24.placement.info(function (info) {
+
+
+            show({
+
+                step: "placement info",
+
+                info: info
+
+            });
+
+
+        });
+
+
+
+    });
+
+
+
+})();
+/*(function () {
+
 "use strict";
 
 
@@ -48,4 +121,4 @@ BX24.init(function () {
 });
 
 
-})();
+})();*/
