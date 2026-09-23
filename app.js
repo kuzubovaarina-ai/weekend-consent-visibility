04f70041e28afc4eef02eb0c3793509f46581988
kuzubovaarina-ai <kuzubovaarina@gmail.com>
2026-09-23 14:31:08 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,52 +1,45 @@
-document.body.innerHTML = `
-<h1>Приложение загрузилось</h1>
-<p>Vercel + Bitrix24 OK</p>
-`;
-/*(function(){
+(function () {
 
 "use strict";
 
 
-function output(data)
-{
-    document.body.innerHTML =
-    `
-    <div style="
-        padding:20px;
-        font-family:Arial;
-        white-space:pre-wrap;
-    ">
-    <h2>Placement debug</h2>
-    <pre>
-${JSON.stringify(data,null,2)}
-    </pre>
-    </div>
+function show(data) {
+
+    document.body.innerHTML = `
+        <div style="
+            padding:20px;
+            font-family:Arial;
+        ">
+            <h2>Bitrix24 Placement Debug</h2>
+
+            <pre>
+${JSON.stringify(data, null, 2)}
+            </pre>
+        </div>
     `;
+
 }
 
 
 
-if(typeof BX24 === "undefined")
-{
+if (typeof BX24 === "undefined") {
 
-    output({
-        error:"BX24 не найден"
+    show({
+        error: "BX24 object not found"
     });
 
     return;
-
 }
 
 
 
-BX24.init(function(){
-
-    console.log("BX24 init OK");
+BX24.init(function () {
 
 
-    BX24.placement.info(function(info){
+    BX24.placement.info(function (info) {
 
-        output(info);
+
+        show(info);
 
 
     });
@@ -55,4 +48,4 @@ BX24.init(function(){
 });
 
 
-})();"*/
+})();
