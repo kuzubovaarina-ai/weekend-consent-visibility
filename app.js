0425a03ccc1f5531ca2b4c81c1e81329ba273a2f
kuzubovaarina-ai <kuzubovaarina@gmail.com>
2026-09-23 14:58:10 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,130 +1,19 @@
-alert("APP JS ЗАГРУЖЕН");
+(function () {
 
-document.body.innerHTML = `
-<h1>JS работает</h1>
-<p>Версия диагностики 001</p>
-`;
-/*(function () {
+    console.log("APP JS START");
 
-    "use strict";
+    const app = document.getElementById("app");
 
-
-    function show(data) {
-
-        document.body.innerHTML = `
-        <div style="
-            padding:20px;
-            font-family:Arial;
-            color:#333;
-        ">
-            <h2>Bitrix24 Debug</h2>
-
-            <pre>
-${JSON.stringify(data, null, 2)}
-            </pre>
-        </div>
+    if (app) {
+        app.innerHTML = `
+            <h1>JS работает</h1>
+            <p>Версия диагностики 002</p>
         `;
-
+    } else {
+        document.body.innerHTML = `
+            <h1>JS работает</h1>
+            <p>Элемент app не найден</p>
+        `;
     }
 
-
-    show({
-        step: "start",
-        bx24: typeof BX24
-    });
-
-
-
-    if (typeof BX24 === "undefined") {
-
-        show({
-            error: "BX24 object not found"
-        });
-
-        return;
-
-    }
-
-
-
-    BX24.init(function () {
-
-
-        show({
-            step: "BX24 init success"
-        });
-
-
-
-        BX24.placement.info(function (info) {
-
-
-            show({
-
-                step: "placement info",
-
-                info: info
-
-            });
-
-
-        });
-
-
-
-    });
-
-
-
-})();*/
-/*(function () {
-
-"use strict";
-
-
-function show(data) {
-
-    document.body.innerHTML = `
-        <div style="
-            padding:20px;
-            font-family:Arial;
-        ">
-            <h2>Bitrix24 Placement Debug</h2>
-
-            <pre>
-${JSON.stringify(data, null, 2)}
-            </pre>
-        </div>
-    `;
-
-}
-
-
-
-if (typeof BX24 === "undefined") {
-
-    show({
-        error: "BX24 object not found"
-    });
-
-    return;
-}
-
-
-
-BX24.init(function () {
-
-
-    BX24.placement.info(function (info) {
-
-
-        show(info);
-
-
-    });
-
-
-});
-
-
-})();*/
+})();
